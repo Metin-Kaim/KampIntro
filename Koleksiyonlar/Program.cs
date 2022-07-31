606b6517616490caaf9632c2759d29f347ee3fae
Metin Kaim <metinkaim0@gmail.com>
2022-07-31 14:24:41 +0300

    16 ocak ders sonu

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "engin", "murat", "kerem", "halil" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+
+            List<string> isimler2 = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+            Console.WriteLine(isimler2[0]);
+        }
+    }
+}
